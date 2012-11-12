cdb449784e474239da7c413a4099d7a1b7443f2c
Bowen Liang <simonlbw@gmail.com>
2012-11-12 21:21:11 +0800

    XXX

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,14 +3,22 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
-import models.*;
+
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
+    public static void index() throws SQLException {
+    	Connection conn=play.db.DB.getConnection();
+    	String str="select * from SCORE_TB";
+    	ResultSet rs = conn.prepareStatement(str).executeQuery();
+    	rs.last(); 
+    	int rowCount = rs.getRow(); //获得ResultSet的总行数 
+        renderText(rowCount);
     }
 
 }
