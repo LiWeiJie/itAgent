2b8b0d72136bef0f2d20d75f86ec699a85de18a4
Bowen Liang <simonlbw@gmail.com>
2012-11-12 21:04:00 +0800

    创建对数据库操作的接口和实现类。

---
+++ app/itagent/dao/ProblemTBServiceImpl.java
@@ -0,0 +1,100 @@
+package itagent.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import play.Logger;
+import play.Play;
+
+public class ProblemTBServiceImpl implements IProblemTBService {
+	public final String PROBLEM_TB_NAME = Play.configuration
+			.getProperty("PROBLEM_TB_NAME");
+
+	@Override
+	public String getSingleProblemById(int Id) {
+		// TODO Auto-generated method stub
+		String problemContent = "";
+		Connection conn = play.db.DB.getConnection();
+		String sql = "SELECT * FROM " + PROBLEM_TB_NAME + " WHERE PROBLEM_ID="
+				+ Id;
+		Logger.debug(sql);
+		try {
+			ResultSet rs = conn.prepareStatement(sql).executeQuery();
+			rs.beforeFirst();
+			while (rs.next())
+				problemContent = rs.getString("Content");
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return problemContent;
+	}
+
+	@Override
+	public String setTeamTime(int problemId, String teamId, Long Time) {
+		// TODO Auto-generated method stub
+		Connection conn = play.db.DB.getConnection();
+		String sql = "UPDATE " + PROBLEM_TB_NAME + " SET " + teamId + " = "
+				+ Time + " WHERE PROBLEM_ID = " + problemId;
+		Logger.debug(sql);
+		try {
+			conn.prepareStatement(sql).execute();
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "FAILED";
+		}
+		return "SUCCESS";
+	}
+
+	@Override
+	public long getTeamTime(int problemId, String teamId) {
+		// TODO Auto-generated method stub
+		long teamTime = -1;
+		Connection conn = play.db.DB.getConnection();
+		String sql = "SELECT * FROM " + PROBLEM_TB_NAME + " WHERE PROBLEM_ID="
+				+ problemId;
+		Logger.debug(sql);
+		try {
+			ResultSet rs = conn.prepareStatement(sql).executeQuery();
+			rs.beforeFirst();
+			while (rs.next()) {
+				teamTime = rs.getLong(teamId);
+				break;
+			}
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return teamTime;
+	}
+
+	@Override
+	public int getProblemsCount() {
+		// TODO Auto-generated method stub
+
+		int totalProblemCount = -1;
+		Connection conn = play.db.DB.getConnection();
+		String sql = "SELECT COUNT(*) FROM " + PROBLEM_TB_NAME;
+		Logger.debug(sql);
+		try {
+			ResultSet rs = conn.prepareStatement(sql).executeQuery();
+			rs.beforeFirst();
+			while (rs.next()) {
+				totalProblemCount = rs.getInt("COUNT(*)");
+				break;
+			}
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return totalProblemCount;
+	}
+
+}
---
+++ app/itagent/dao/ScoreTBServiceImpl.java
@@ -0,0 +1,55 @@
+package itagent.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import play.Logger;
+import play.Play;
+
+public class ScoreTBServiceImpl implements IScoreTBService {
+	public final String SCORE_TB_NAME = Play.configuration
+			.getProperty("SCORE_TB_NAME");
+
+	@Override
+	public String setTeamScore(String teamId, int Score) {
+		// TODO Auto-generated method stub
+		Connection conn = play.db.DB.getConnection();
+		String sql = "UPDATE " + SCORE_TB_NAME + " SET TEAM_SCORE=" + Score
+				+ " WHERE TEAM_ID = '" + teamId+"';";
+		Logger.debug(sql);
+		try {
+			conn.prepareStatement(sql).execute();
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "FAILED";
+		}
+		return "SUCCESS";
+	}
+
+	@Override
+	public int getTeamScore(String teamId) {
+		// TODO Auto-generated method stub
+		int teamScore = -1;
+		Connection conn = play.db.DB.getConnection();
+		String sql = "SELECT * FROM " + SCORE_TB_NAME + " WHERE TEAM_ID='"
+				+ teamId + "';";
+		System.out.println(sql);
+		try {
+			ResultSet rs = conn.prepareStatement(sql).executeQuery();
+			rs.beforeFirst();
+			while (rs.next()) {
+				teamScore = rs.getInt("TEAM_SCORE");
+				break;
+			}
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return teamScore;
+	}
+
+}
