bff52994f2d7047fdcb62612549474e56c8b98f6
liweijie <fenrir1205@gmail.com>
2012-11-13 00:44:37 +0800

    把后台的数据接口写好了

--- app/controllers/Console.java
+++ app/controllers/Console.java
@@ -16,17 +16,10 @@ public class Console extends Controller {
 	static IProblemTBService problemTBService;
 	@Inject
 	static IScoreTBService scoreTBService;
-	public static void index() throws SQLException {
-		int str =problemTBService.getProblemsCount();
-        renderText(str);
-    }
-	
-	public static void getProblem(int id) throws SQLException {
-		int str =problemTBService.getProblemsCount();
-        renderText(str);
-    }
 	
 	private static int loadingQue = 0;
+	
+	private static String[] teamIds = {"T1","T2","T3"};
 	
 	public static void startQues(int quesId)
 	{
@@ -34,4 +27,40 @@ public class Console extends Controller {
 		loadingQue = quesId;
 		renderText(quesId);
 	}
+	
+	public static void index() throws SQLException {
+		int str =problemTBService.getProblemsCount();
+        renderText(str);
+    }
+	
+	public static void getProblem() throws SQLException {
+		String str =problemTBService.getSingleProblemById(loadingQue);
+        renderText(str);
+    }
+	
+	public static void setTeamAnswerTime(int problemId,int teamId) throws SQLException {
+		String str =problemTBService.setTeamTime(problemId, teamIds[teamId], System.currentTimeMillis());
+        renderText(str);
+    }
+	
+	public static void setTeamScore(int teamId,int plus) throws SQLException {
+		String str =scoreTBService.setTeamScore(teamIds[teamId], scoreTBService.getTeamScore(teamIds[teamId])+plus);
+		renderText(str);
+    }
+	
+	public static void getTeamScore(int teamId) throws SQLException {
+		long str = scoreTBService.getTeamScore(teamIds[teamId]);
+		renderText(str);
+    }
+	
+	public static void getTimeRanking(int no) throws SQLException {
+		long[] score=new long[9];
+		for(int i=0;i<9;i++)
+		{
+			score[i] = scoreTBService.getTeamScore(teamIds[i]); 
+		}
+		
+    }
+	
+
 }
